a04fa805e362e66fb62c694361ab9207e3d39ef2
Jon Meyers <jonathon.d.meyers@gmail.com>
2023-07-27 10:30:51 +1000

    add force-dynamic

--- app/get-cookie-edge/route.ts
+++ app/get-cookie-edge/route.ts
@@ -2,6 +2,7 @@ import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
 export const runtime = "edge";
+export const dynamic = "force-dynamic";
 
 export async function GET() {
   console.log(cookies().getAll());
--- app/set-cookie/route.ts
+++ app/set-cookie/route.ts
@@ -1,6 +1,8 @@
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
+export const dynamic = "force-dynamic";
+
 export async function GET() {
   cookies().set("cookie", "value");
   return redirect("/");
