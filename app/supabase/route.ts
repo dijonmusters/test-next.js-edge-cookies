693467b65dd5739242f997eaa0936a95845b53fa
Jon Meyers <jonathon.d.meyers@gmail.com>
2023-07-28 20:45:47 +1000

    simplify example

--- app/supabase/route.ts
+++ app/supabase/route.ts
@@ -1,22 +1,20 @@
 import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
-import { NextResponse } from "next/server";
+import { redirect } from "next/navigation";
 
 export const runtime = "edge";
 export const dynamic = "force-dynamic";
 
-export async function GET(request: Request) {
-  const requestUrl = new URL(request.url);
+export async function GET() {
   const cookieStore = cookies();
 
   const supabase = createRouteHandlerClient({
     cookies: () => cookieStore,
   });
-  const { data, error } = await supabase
-    .from("tests")
-    .insert({ title: "outside stream" });
+
+  const { data, error } = await supabase.auth.getSession();
 
   console.log({ data, error });
 
-  return NextResponse.redirect(requestUrl.origin);
+  return redirect("/");
 }
