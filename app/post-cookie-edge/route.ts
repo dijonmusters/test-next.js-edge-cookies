f4a770f870acef7146d0c4ee3ae061e68af351bc
Jon Meyers <jonathon.d.meyers@gmail.com>
2023-07-28 19:38:46 +1000

    add redirect

--- app/post-cookie-edge/route.ts
+++ app/post-cookie-edge/route.ts
@@ -1,7 +1,7 @@
 import { cookies } from "next/headers";
-import { redirect } from "next/navigation";
 import wait from "../_utils/wait";
 import getInstance from "../_utils/instance";
+import { NextResponse } from "next/server";
 
 export const runtime = "edge";
 
@@ -9,5 +9,7 @@ export async function POST() {
   const cookieInstance = getInstance({ cookies });
   await wait(1000);
   console.log(cookieInstance.getCookie("cookie"));
-  return redirect("/");
+  return NextResponse.redirect("http://localhost:3000/", {
+    status: 301,
+  });
 }
