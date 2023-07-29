af5707c3f9d64c6eac7fe91f34a85275d5755e95
Jon Meyers <jonathon.d.meyers@gmail.com>
2023-07-29 20:09:25 +1000

    upgrade to canary

--- app/page.tsx
+++ app/page.tsx
@@ -5,19 +5,23 @@ export const runtime = "edge";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <Link href="/working-node-route-handler">Working Node Route Handler</Link>
-      <Link href="/working-node-server-component">
-        Working Node Server Component
+      <Link href="/working-node-route-handler">
+        ✅ Working Node Route Handler
       </Link>
-      <Link href="/working-edge-route-handler">Working Edge Route Handler</Link>
+      <Link href="/working-node-server-component">
+        ✅ Working Node Server Component
+      </Link>
+      <Link href="/working-edge-route-handler">
+        ✅ Working Edge Route Handler
+      </Link>
       <Link href="/working-edge-server-component">
-        Working Edge Server Component
+        ✅ Working Edge Server Component
       </Link>
       <Link href="/not-working-edge-route-handler">
-        Not Working Edge Route Handler
+        ❌ Not Working Edge Route Handler
       </Link>
       <Link href="/not-working-edge-server-component">
-        Not Working Edge Server Component
+        ❌ Not Working Edge Server Component
       </Link>
     </main>
   );
